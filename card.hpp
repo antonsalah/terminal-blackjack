75901326d6155798a71c1d38cb7e8e6925177da4
Anton Salah <antonsalah47@gmail.com>
2024-08-21 23:48:20 -0700

    inital commit

---
+++ card.hpp
@@ -0,0 +1,34 @@
+enum Suit{
+    heart = 0,
+    diamond = 1,   
+    club = 2,
+    spade = 3
+};
+
+class Card {
+
+    public:
+        char value;
+        Suit suit;
+
+        void printCard(Card card){
+            //TODO
+        }
+        
+        void setValue(char value){
+            //TODO
+        }
+
+        void setSuit(Suit suit){
+            //TODO
+        }
+
+        int getValue(){
+            //TODO
+        }
+        Suit getSuit(){
+            //TODO
+        }
+
+
+};
