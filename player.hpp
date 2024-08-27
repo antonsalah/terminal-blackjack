71161339a8bad1af4140477f7e0b514857eb05f1
Anton Salah <antonsalah47@gmail.com>
2024-08-27 11:33:20 -0700

    added structure of Player class

---
+++ player.hpp
@@ -0,0 +1,16 @@
+#include "card.hpp"
+
+class Player{
+    private:
+    Card hand[20];
+
+    public:
+
+    void drawCard(){
+        //todo
+    }
+
+    void clearHand(){
+        //todo
+    }
+};
