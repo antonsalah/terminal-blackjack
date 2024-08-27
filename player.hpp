0ee05e9bca0f8f686c36913144afc97b1edc1985
Anton Salah <antonsalah47@gmail.com>
2024-08-27 12:48:46 -0700

    added draw random card function

--- player.hpp
+++ player.hpp
@@ -1,3 +1,5 @@
+#include <ctime>
+#include <cstdlib>
 #include "card.hpp"
 
 class Player{
@@ -6,8 +8,20 @@ class Player{
 
     public:
 
+    void addCard(Card newCard){
+
+    }
+
     void drawCard(){
-        //todo
+        //do check handeling. IE see if the hand if full and do stuff if so.
+        std::srand((unsigned)std::time(NULL));
+        int randNumGen = rand() % 52; 
+
+        Suit s = static_cast<Suit>(randNumGen % 4);
+        Value val = static_cast<Value>(randNumGen % 13);
+
+        Card newCard(s,val);
+        //addCard(newCard);
     }
 
     void clearHand(){
