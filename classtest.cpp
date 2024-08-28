48492936a4a0bae78faa8a960fab2c662d06226b
Anton Salah <antonsalah47@gmail.com>
2024-08-28 16:23:22 -0700

    added testing for score keeping features in player class

--- classtest.cpp
+++ classtest.cpp
@@ -7,9 +7,9 @@
 #include <fcntl.h>
 #include <assert.h>
 int main(){
-    Card test(Suit::spade,Value::ace);
     _setmode(_fileno(stdout), _O_U16TEXT);
-
+    
+    Card test(Suit::spade,Value::ace);
     Suit testSuit = test.getSuit();
     assert(testSuit == spade);
     std::wcout << "Test 1 passed!" << std::endl;
@@ -31,17 +31,28 @@ int main(){
     std::wcout << "Test 4 Passed!" << std::endl;
 
     Player testPlayer;
+    unsigned runningScore = 0;
     testPlayer.drawCard();
+    runningScore += testPlayer.getLatestCard()->getValue();
+    assert(testPlayer.getScore() ==runningScore);
+    std::wcout << "Test 5.1 Passed!" << std::endl;
     testPlayer.drawCard();
+    runningScore += testPlayer.getLatestCard()->getValue();
+    assert(testPlayer.getScore() == runningScore);
+    std::wcout << "Test 5.2 Passed!" << std::endl;
     testPlayer.drawCard();
+    runningScore += testPlayer.getLatestCard()->getValue();
+    assert(testPlayer.getScore() == runningScore);
+    std::wcout << "Test 5.3 Passed!" << std::endl;
     testPlayer.printHand();
 
-    std::wcout << "Test 5 Passed!" << std::endl;
+    std::wcout << "All of Test 5 Passed!" << std::endl;
 
     testPlayer.clearHand();
     testPlayer.printHand();
-
-    std::wcout << "Test 6 Passed!" << std::endl;
+    std::wcout << "Test 6.1 Passed!" << std::endl;
+    assert(testPlayer.getScore() == 0);
+    std::wcout << "All of Tests 6 Passed!" << std::endl;
 
     return 0;
 }
